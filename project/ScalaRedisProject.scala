3146916f494985f3e30124d707b3eca22126a5a7
weakwire <papadaks@gmail.com>
2013-07-05 00:02:19 +0300

    no publish

--- project/ScalaRedisProject.scala
+++ project/ScalaRedisProject.scala
@@ -44,40 +44,8 @@ object ScalaRedisProject extends Build
           "log4j"             %  "log4j"                   % "1.2.16"     % "provided",
           "junit"             %  "junit"                   % "4.8.1"      % "test",
           "org.scalatest"     %  ("scalatest_" + v)          % "2.0.M4"      % "test")
-    },
-
-    parallelExecution in Test := false,
-    publishTo <<= version { (v: String) => 
-      val nexus = "https://oss.sonatype.org/" 
-      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
-      else Some("releases" at nexus + "service/local/staging/deploy/maven2") 
-    },
-    credentials += Credentials(Path.userHome / ".sbt" / "sonatype.credentials"),
-    publishMavenStyle := true,
-    publishArtifact in Test := false,
-    pomIncludeRepository := { repo => false },
-    pomExtra := (
-      <url>https://github.com/debasishg/scala-redis</url>
-      <licenses>
-        <license>
-          <name>Apache 2.0 License</name>
-          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-      <scm>
-        <url>git@github.com:debasishg/scala-redis.git</url>
-        <connection>scm:git:git@github.com:debasishg/scala-redis.git</connection>
-      </scm>
-      <developers>
-        <developer>
-          <id>debasishg</id>
-          <name>Debasish Ghosh</name>
-          <url>http://debasishg.blogspot.com</url>
-        </developer>
-      </developers>),
-    unmanagedResources in Compile <+= baseDirectory map { _ / "LICENSE" }
-  )
+    }   
+   
 }
 
 object Resolvers {
